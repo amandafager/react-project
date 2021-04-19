c87518ef9622cf62bef8c035cc4ce8e5dd0c7e4c
amanda.fager@outlook.com <amanda.fager@outlook.com>
2021-04-19 16:55:45 +0200

    testing tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,3 +9,21 @@ module.exports = {
   },
   plugins: [],
 };
+
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      green: colors.emerald,
+    },
+  },
+};
