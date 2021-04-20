cb948c17b0893d1aa4d07eba134bee203570dfc3
amanda.fager@outlook.com <amanda.fager@outlook.com>
2021-04-20 11:55:15 +0200

    added a small change in layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,5 +1,5 @@
 function Layout(props) {
-  return <div className="m-0 box-border mb-16">{props.children}</div>;
+  return <div className="m-0 box-border mb-16 h-screen">{props.children}</div>;
 }
 
 export default Layout;
